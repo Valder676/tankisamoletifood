5f850231eff41fd5ed4f860faf81a5e7d809260a
123 <skjk@mail.ru>
2023-12-19 12:49:42 +0300

    gogogo
    gogogo

---
+++ tankionline/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+interface IDrive
+{
+    string Drive();
+}
+
+interface IRotateTurret
+{
+    string RotateTurret();
+}
+
+interface IShoot
+{
+    string Shoot();
+}
+
+interface ISwim
+{
+    string Swim();
+}
+
+
+abstract class Vehicle
+{
+    public abstract string StartEngine();
+}
+
+class Car : Vehicle, IDrive
+{
+    public override string StartEngine()
+    {
+        return "Двигатель автомобиля заведен!";
+    }
+
+    public string Drive()
+    {
+        return "Автомобиль едет.";
+    }
+}
+
+
+class Motorcycle : Vehicle, IDrive
+{
+    public override string StartEngine()
+    {
+        return "Двигатель мотоцикла заведен!";
+    }
+
+    public string Drive()
+    {
+        return "Мотоцикл едет.";
+    }
+}
+
+
+class Boat : Vehicle, ISwim
+{
+    public override string StartEngine()
+    {
+        return "Двигатель лодки заведен!";
+    }
+
+    public string Swim()
+    {
+        return "Лодка плывет.";
+    }
+}
+
+class Tank : Vehicle, IDrive, IRotateTurret, IShoot
+{
+    public override string StartEngine()
+    {
+        return "Двигатель танка заведен!";
+    }
+
+    public string Drive()
+    {
+        return "Танк едет.";
+    }
+
+    public string RotateTurret()
+    {
+        return "Танк вращает башней.";
+    }
+
+    public string Shoot()
+    {
+        return "Танк стреляет.";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Vehicle> vehicles = new List<Vehicle>
+        {
+            new Car(),
+            new Motorcycle(),
+            new Boat(),
+            new Tank()
+        };
+
+
+        foreach (Vehicle vehicle in vehicles)
+        {
+            Console.WriteLine(vehicle.StartEngine());
+        }
+
+        foreach (var vehicle in vehicles)
+        {
+            if (vehicle is IDrive drivable)
+            {
+                Console.WriteLine(drivable.Drive());
+            }
+            if (vehicle is IRotateTurret turretRotatable)
+            {
+                Console.WriteLine(turretRotatable.RotateTurret());
+            }
+            if (vehicle is IShoot shootable)
+            {
+                Console.WriteLine(shootable.Shoot());
+            }
+            if (vehicle is ISwim swimmable)
+            {
+                Console.WriteLine(swimmable.Swim());
+            }
+        }
+    }
+}
---
+++ tankionline/food(sluchaino sozdal vse vmeste)/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+interface IHot
+{
+    string ServeHot();
+}
+
+interface ICold
+{
+    string ServeCold();
+}
+
+interface IVegetarian
+{
+    string ServeVegetarian();
+}
+
+interface IMeatDish
+{
+    string ServeMeat();
+}
+
+
+abstract class Food
+{
+    public string Name { get; }
+
+    protected Food(string name)
+    {
+        Name = name;
+    }
+
+    public abstract string Consume();
+}
+
+
+class Soup : Food, IHot
+{
+    public Soup(string name) : base(name) { }
+
+    public override string Consume()
+    {
+        return $"Вы едите горячий суп {Name}.";
+    }
+
+    public string ServeHot()
+    {
+        return $"Суп {Name} подается горячим.";
+    }
+}
+
+
+class Salad : Food, ICold
+{
+    public Salad(string name) : base(name) { }
+
+    public override string Consume()
+    {
+        return $"Вы едите свежий салат {Name}.";
+    }
+
+    public string ServeCold()
+    {
+        return $"Салат {Name} подается холодным.";
+    }
+}
+
+
+class Steak : Food, IHot, IMeatDish
+{
+    public Steak(string name) : base(name) { }
+
+    public override string Consume()
+    {
+        return $"Вы наслаждаетесь стейком {Name}.";
+    }
+
+    public string ServeHot()
+    {
+        return $"Стейк {Name} подается горячим.";
+    }
+
+    public string ServeMeat()
+    {
+        return $"Стейк {Name} - блюдо из мяса.";
+    }
+}
+
+class CanteenProgram
+{
+    static void Main()
+    {
+        List<Food> menu = new List<Food>
+        {
+            new Soup("Борщ"),
+            new Salad("Цезарь"),
+            new Steak("Рибай")
+        };
+
+        foreach (var dish in menu)
+        {
+            Console.WriteLine(dish.Consume());
+
+            if (dish is IHot hotDish)
+            {
+                Console.WriteLine(hotDish.ServeHot());
+            }
+            if (dish is ICold coldDish)
+            {
+                Console.WriteLine(coldDish.ServeCold());
+            }
+            if (dish is IVegetarian vegetarianDish)
+            {
+                Console.WriteLine(vegetarianDish.ServeVegetarian());
+            }
+            if (dish is IMeatDish meatDish)
+            {
+                Console.WriteLine(meatDish.ServeMeat());
+            }
+        }
+    }
+}
